0cdfcf512f0fb0c8d5f48fd4b2d651de3f1a7c3b
sohyun <shlim0410@gmail.com>
2023-06-01 17:10:55 +0100

    feat: fetch coins data

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ const GlobalStyle = createGlobalStyle`
   }
   a {
     text-decoration: none;
-
+    color: inherit;
   }
 `;
 
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,97 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 10px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header``;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.bgColor};
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    border-radius: 15px;
+    transition: background-color 0.2s ease-in;
+  }
+  border-radius: 15px;
+  margin: 10px;
+  &:hover {
+    a {
+      background-color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+  text-align: center;
 `;
+
+const Loader = styled.div`
+  text-align: center;
+  padding: 50px 0px;
+`;
+
+const Img = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <Title>Coin</Title>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <Img
+                    src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                  />
+                  {coin.name}
+                </div>
+                <div>➔</div>{" "}
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
